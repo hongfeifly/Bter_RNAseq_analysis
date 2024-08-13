9e269c04db089e40d6e2816a5a36df5e2b100922
hongfeifly <118990639+hongfeifly@users.noreply.github.com>
2024-08-13 14:04:56 +0200

    Update GO_KS_brain_worker.R

--- Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_caste_biassed_genes/GO_KS_brain_worker.R
+++ Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_caste_biassed_genes/GO_KS_brain_worker.R
@@ -1,5 +1,5 @@
 #####
-# author: "Joe Colgan", "Hongfei Xu"
+# author: "Joe Colgan" (joscolgan), "Hongfei Xu" (hongfeifly)
 # title: "**Gene Ontology term enrichment analysis of worker-biassed genes
 # in brain of _Bombus terrestris_**"
 # output:
