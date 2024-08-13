071670736f8c4a00614b3210137d51e287dbee92
hongfeifly <118990639+hongfeifly@users.noreply.github.com>
2024-08-13 14:04:25 +0200

    Update GO_fisher_BSPs.R

--- Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_BSPs/GO_fisher_BSPs.R
+++ Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_BSPs/GO_fisher_BSPs.R
@@ -1,5 +1,5 @@
 #####
-# author: "Joe Colgan", "Hongfei Xu"
+# author: "Joe Colgan" (joscolgan), "Hongfei Xu" (hongfeifly)
 # title: "**Gene Ontology term enrichment analysis of BSPs of _Bombus terrestris_**"
 # output:
 #   pdf_document: default
