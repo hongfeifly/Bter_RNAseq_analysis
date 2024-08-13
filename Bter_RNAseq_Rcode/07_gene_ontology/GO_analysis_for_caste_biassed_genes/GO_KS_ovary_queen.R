a92d21ab7c831f3cc3d86eb410cfaf90f8b55d6b
hongfeifly <118990639+hongfeifly@users.noreply.github.com>
2024-08-13 14:05:48 +0200

    Update GO_KS_ovary_queen.R

--- Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_caste_biassed_genes/GO_KS_ovary_queen.R
+++ Bter_RNAseq_Rcode/07_gene_ontology/GO_analysis_for_caste_biassed_genes/GO_KS_ovary_queen.R
@@ -1,5 +1,5 @@
 #####
-# author: "Joe Colgan", "Hongfei Xu"
+# author: "Joe Colgan" (joscolgan), "Hongfei Xu" (hongfeifly)
 # title: "**Gene Ontology term enrichment analysis of queen-biassed genes
 # in ovary of _Bombus terrestris_**"
 # output:
